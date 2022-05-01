32fe347f94ebc6d137739820dacd5840848d6baf
Erik Neves <erikrrn04@gmail.com>
2022-05-01 13:25:24 -0300

    AccessLogger Repository

---
+++ Access/Data/Repository/AccessLoggerRepository.cs
@@ -0,0 +1,11 @@
+﻿using Access.Interfaces;
+using Access.Models.Base;
+
+namespace Access.Data.Repository;
+
+public class AccessLoggerRepository : Repository<AccessLogger>, IAccessLoggerRepository
+{
+    public AccessLoggerRepository(Context context) : base(context)
+    {
+    }
+}
