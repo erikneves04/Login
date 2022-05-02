32f5f57c86e749a41ac6b70f593c2c0b84b48a4c
Erik Neves <erikrrn04@gmail.com>
2022-05-01 21:17:06 -0300

    Update IUserServices.cs

--- Access/Interfaces/Services/IUserServices.cs
+++ Access/Interfaces/Services/IUserServices.cs
@@ -1,9 +1,11 @@
-﻿using Access.Models.View;
+﻿using Access.Models.Base;
+using Access.Models.View;
 
 namespace Access.Interfaces.Services;
 
 public interface IUserServices
 {
+    User Access(string email, string password);
     void Delete(Guid id);
     UserView Insert(UserInsertView data);
     UserView Update(UserInsertView data, Guid id);
